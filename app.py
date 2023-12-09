0b4455f165df75b85425b174f1b99b0aabe1412e
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-09 05:22:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time 
 import plotly.express as px
 import setuptools
-from streamlit_plotly_events import plotly_events
+
 
 # Example data for language model metrics
 language_metrics_data = {
